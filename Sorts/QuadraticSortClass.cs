e0c673d7cafda0375d5afcfa9ac9778633a2df30
Paulo Renato <seven.renato@hotmail.com>
2024-02-12 23:42:04 -0300

    Add project files.

---
+++ Sorts/QuadraticSortClass.cs
@@ -0,0 +1,44 @@
+﻿namespace DataStructuresC_.Sorts;
+public class QuadraticSortClass
+{
+    int[] Vet {  get; set; }
+    public QuadraticSortClass(int[] vet, string name)
+    {
+        Vet = vet; 
+    }
+
+    public int[] SelectionSort()
+    {
+        int[] UnsortedList = Vet;
+        int[] SortedList = new int[UnsortedList.Length];
+
+        Array.Copy(UnsortedList, SortedList, UnsortedList.Length); // Copiar para manipular longe da Lista base que foi enviada!
+
+
+
+        return SortedList;
+    }
+    public int[] InsertionSort()
+    {
+        int[] UnsortedList = Vet;
+        int[] SortedList = new int[UnsortedList.Length];
+
+        Array.Copy(UnsortedList, SortedList, UnsortedList.Length); // Copiar para manipular longe da Lista base que foi enviada!
+
+
+
+        return SortedList;
+    }
+
+    public int[] BubbleSort()
+    {
+        int[] UnsortedList = Vet;
+        int[] SortedList = new int[UnsortedList.Length];
+
+        Array.Copy(UnsortedList, SortedList, UnsortedList.Length); // Copiar para manipular longe da Lista base que foi enviada!
+
+
+
+        return SortedList;
+    }
+}
